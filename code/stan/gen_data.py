4fe32c8708de02b25b402d876ef8dc8ade01d855
Dawen Liang <dl2771@columbia.edu>
2013-06-15 00:44:53 -0700

    Added arguments for gen_data.py

--- code/stan/gen_data.py
+++ code/stan/gen_data.py
@@ -1,23 +1,26 @@
+import sys
 import scipy.io as sio
 
-L = 40
-d = sio.loadmat('sa1.mat')
-V = d['V']
+def gen_data(matfile, L):
+    d = sio.loadmat(matfile)
+    V = d['V']
+    F, T = V.shape
 
-F, T = V.shape
+    fout = open('full_bayes.data.R', 'w')
+    fout.write('F <- {}\n'.format(F))
+    fout.write('T <- {}\n'.format(T))
+    fout.write('L <- {}\n'.format(L))
+    fout.write('V <- structure(c(')
+    for f in xrange(F):
+        for t in xrange(T):
+            fout.write(str(V[f,t]))
+            if f < F-1 or t < T-1:
+                fout.write(', ')
+    fout.write('), .Dim = c({}, {}))\n'.format(T, F))
+    fout.close()
+    pass
 
-fout = open('full_bayes.data.R', 'w')
-
-fout.write('F <- {}\n'.format(F))
-fout.write('T <- {}\n'.format(T))
-fout.write('L <- {}\n'.format(L))
-fout.write('V <- structure(c(')
-
-for f in xrange(F):
-    for t in xrange(T):
-        fout.write(str(V[f,t]))
-        if f < F-1 or t < T-1:
-            fout.write(', ')
-
-fout.write('), .Dim = c({}, {}))\n'.format(T, F))
-fout.close()
+if __name__ == '__main__':
+    matfile = sys.argv[1]
+    L = sys.argv[2]
+    gen_data(matfile, L)
