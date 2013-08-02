1fdce33ea8484dddc6b940d388bcf429a746f935
Dawen Liang <dl2771@columbia.edu>
2013-08-02 00:16:22 -0700

    sync

--- code/exp_sf_gap_nmf.py
+++ code/exp_sf_gap_nmf.py
@@ -3,18 +3,147 @@
 
 # <codecell>
 
-import librosa, sf_gap_nmf
+import functools, glob, pickle
+
+from scikits.audiolab import Sndfile, Format
+
+import librosa, gap_nmf, sf_gap_nmf
 import scipy.io as sio
 
 # <codecell>
 
+fig = functools.partial(figure, figsize=(16,4))
+specshow = functools.partial(imshow, cmap=cm.hot_r, aspect='auto', origin='lower', interpolation='nearest')
+
+def logspec(X, amin=1e-10, dbdown=80):
+    logX = 20 * np.log10(np.maximum(X, amin))
+    return np.maximum(logX, logX.max() - dbdown)
+
+def save_object(obj, filename):
+    with open(filename, 'wb') as output:
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+    pass
+
+def load_object(filename):
+    with open(filename, 'r') as output:
+        obj = pickle.load(output)
+    return obj 
+
+# <codecell>
+
 d = sio.loadmat('log_normal_gender.mat')
-U = d['U']
+U = d['U'].T
 gamma = d['gamma']
-alpha = d['alpha']
+alpha = d['alpha'].ravel()
+
+print U.shape
 
 # <codecell>
 
 # if loading priors trained from log-normal, transfer gamma to approximate gamma noise model
-gamma = 
+gamma = 1./(np.exp(2./gamma) - np.exp(1./gamma))
+
+# <codecell>
+
+plot(gamma)
+
+# <codecell>
+
+TIMIT_DIR = '../../timit/train/'
+
+# <codecell>
+
+def load_timit(wav_dir):
+    f = Sndfile(wav_dir, 'r')
+    wav = f.read_frames(f.nframes)
+    return (wav, f.samplerate)
+    
+def write_wav(w, filename, channels=1, samplerate=16000):
+    f_out = Sndfile(filename, 'w', format=Format(), channels=channels, samplerate=samplerate)
+    f_out.write_frames(w)
+    f_out.close()
+    pass
+
+# <codecell>
+
+gender = 'f'
+dirs = !ls -d "$TIMIT_DIR"dr1/"$gender"*
+
+files = [glob.glob(spk_dir + '/*.wav') for spk_dir in dirs]
+n_files = len(files)
+
+# <codecell>
+
+n_fft = 1024
+hop_length = 512
+
+N_train = int(0.8 * n_files)
+N_test = n_files - N_train
+np.random.seed(98765)
+
+idx = np.random.permutation(n_files)
+
+# 80% of the data is used to learn the prior via empirical bayes
+W_complex_test = None
+for file_dir in files[N_train:]:
+    for wav_dir in file_dir:
+        wav, sr = load_timit(wav_dir)
+        if W_complex_test is None:
+            W_complex_test = librosa.stft(wav, n_fft=n_fft, hop_length=hop_length)
+        #else:
+        #    W_complex_test = np.hstack((W_complex_test, librosa.stft(wav, n_fft=n_fft, hop_length=hop_length)))
+
+# <codecell>
+
+specshow(logspec(np.abs(W_complex_test)))
+colorbar()
+pass
+
+# <codecell>
+
+reload(sf_gap_nmf)
+sf_gap = sf_gap_nmf.SF_GaP_NMF(np.abs(W_complex_test), U, gamma, alpha, K=50, seed=98765)
+
+score = -np.inf
+criterion = 0.0005
+for i in xrange(5):
+    sf_gap.update()
+    lastscore = score
+    score = sf_gap.bound()
+    improvement = (score - lastscore) / np.abs(lastscore)
+    print ('iteration {}: bound = {:.2f} ({:.5f} improvement)'.format(i, score, improvement))
+    if improvement < criterion:
+        break
+
+# <codecell>
+
+i
+
+# <codecell>
+
+fig(figsize=(16, 10))
+sf_gap.figures()
+
+# <codecell>
+
+gap = gap_nmf.GaP_NMF(np.abs(W_complex_test), K=50, seed=98765)
+
+score = -np.inf
+criterion = 0.0005
+for i in xrange(1):
+    gap.update()
+    lastscore = score
+    score = gap.bound()
+    improvement = (score - lastscore) / np.abs(lastscore)
+    print ('iteration {}: bound = {:.2f} ({:.5f} improvement)'.format(i, score, improvement))
+    if improvement < criterion:
+        break
+
+# <codecell>
+
+fig(figsize=(16, 10))
+gap.figures()
+
+# <codecell>
+
 
--- code/sf_gap_nmf.py
+++ code/sf_gap_nmf.py
@@ -85,19 +85,20 @@ class SF_GaP_NMF(gap_nmf.GaP_NMF):
         self.Ea, self.Eloga = _gap.compute_gamma_expectation(self.nua,
                                                              self.rhoa)
 
-    def update(self):
+    def update(self, disp=0):
         ''' Do optimization for one iteration
         '''
         self.update_h()
         goodk = self.goodk()
         for k in goodk:
-            self.update_a(k)
+            print k
+            self.update_a(k, disp)
         self.update_w()
         self.update_theta()
         # truncate unused components
         self.clear_badk()
 
-    def update_a(self, k):
+    def update_a(self, k, disp):
         def f(theta):
             nu, rho = np.exp(theta[:self.L]), np.exp(theta[-self.L:])
             Ea, Eloga = _gap.compute_gamma_expectation(nu, rho)
@@ -106,7 +107,7 @@ class SF_GaP_NMF(gap_nmf.GaP_NMF):
             val = val + np.sum(special.gammaln(nu) - nu * np.log(rho))
 
             logEexp = _gap.comp_log_exp(nu, rho, self.U)
-            val = val - np.sum(self.gamma * self.Ew[:, k] *
+            val = val - np.sum(self.gamma.ravel() * self.Ew[:, k] *
                                np.exp(np.sum(logEexp, axis=1)))
             val = val - np.sum(self.gamma * np.dot(self.U, Ea))
             return -val
@@ -126,42 +127,47 @@ class SF_GaP_NMF(gap_nmf.GaP_NMF):
             log_term[-idx] = -np.inf
             inv_term[idx], inv_term[-idx] = 1. / (1. + tmp[idx]), np.inf
 
-            grad_nu = np.sum((self.Ew[:, k] * log_term *
-                              np.exp(np.sum(logEexp, axis=1))
+            grad_nu = np.sum((self.Ew[:, k, np.newaxis] * log_term *
+                              np.exp(np.sum(logEexp, axis=1, keepdims=True))
                               - self.U / rho) * self.gamma, axis=0)
             grad_nu = grad_nu + 1 - self.alpha / rho
             grad_nu = grad_nu + (self.alpha - nu) * special.polygamma(1, nu)
 
-            grad_rho = nu / rho**2 * np.sum((-self.U * self.Ew[:, k] * inv_term
-                                             * np.exp(np.sum(logEexp, axis=1))
+            grad_rho = nu / rho**2 * np.sum((-self.U * self.Ew[:, k, np.newaxis]
+                                             * inv_term
+                                             * np.exp(np.sum(logEexp, axis=1,
+                                                             keepdims=True))
                                              + self.U) * self.gamma, axis=0)
             grad_rho = grad_rho + self.alpha * (nu / rho**2 - 1. / rho)
             return -np.hstack((nu * grad_nu, rho * grad_rho))
 
-        theta0 = np.hstack((np.log(self.nua[:, k]), np.log(self.rho[:, k])))
+        theta0 = np.hstack((np.log(self.nua[:, k]), np.log(self.rhoa[:, k])))
         theta_hat, _, d = optimize.fmin_l_bfgs_b(f, theta0, fprime=df, disp=0)
-        if d['warnflag']:
+        if disp and d['warnflag']:
             if d['warnflag'] == 2:
                 print 'A[:, {}]: {}, f={}'.format(k, d['task'],
                                                   f(theta_hat))
             else:
                 print 'A[:, {}]: {}, f={}'.format(k, d['warnflag'],
                                                   f(theta_hat))
-        #    app_grad = approx_grad(f, theta_hat)
-        #    for l in xrange(self.L):
-        #        print_gradient('log_a[{}, {:3d}]'.format(t, l), theta_hat[l],
-        #                df(theta_hat)[l], app_grad[l])
-        #        print_gradient('log_b[{}, {:3d}]'.format(t, l), theta_hat[l +
-        #            self.L], df(theta_hat)[l + self.L], app_grad[l + self.L])
+            app_grad = approx_grad(f, theta_hat)
+            ana_grad = df(theta_hat)
+            for l in xrange(self.L):
+                print_gradient('log_a[{}, {:3d}]'.format(l, k), theta_hat[l],
+                               ana_grad[l], app_grad[l])
+                print_gradient('log_b[{}, {:3d}]'.format(l, k),
+                               theta_hat[l + self.L], ana_grad[l + self.L],
+                               app_grad[l + self.L])
 
-        self.nua[:, k], self.b[:, k] = np.exp(theta_hat[:self.L]), np.exp(
+        self.nua[:, k], self.rhoa[:, k] = np.exp(theta_hat[:self.L]), np.exp(
             theta_hat[-self.L:])
         assert(np.all(self.nua[:, k] > 0))
         assert(np.all(self.rhoa[:, k] > 0))
         self.Ea[:, k], self.Eloga[:, k] = _gap.compute_gamma_expectation(
             self.nua[:, k], self.rhoa[:, k])
         self.logEexpa[:, :, k] = _gap.comp_log_exp(self.nua[:, k],
-                                                   self.rhoa[:, k], self.U)
+                                                   self.rhoa[:, k],
+                                                   self.U)
 
     def update_w(self):
         goodk = self.goodk()
@@ -288,3 +294,18 @@ class SF_GaP_NMF(gap_nmf.GaP_NMF):
         dEtinvinv = self.Etinvinv[goodk]
         return np.dot(self.Ewinvinv[:, goodk],
                       dEtinvinv[:, np.newaxis] * self.Ehinvinv[goodk, :])
+
+
+def approx_grad(f, x, delta=1e-8, args=()):
+    x = np.asarray(x).ravel()
+    grad = np.zeros_like(x)
+    diff = delta * np.eye(x.size)
+    for i in xrange(x.size):
+        grad[i] = (f(x + diff[i], *args) - f(x - diff[i], *args)) / (2 * delta)
+    return grad
+
+
+def print_gradient(name, val, grad, approx):
+    print('{} = {:.2f}\tGradient: {:.2f}\tApprox: {:.2f}\t'
+          '| Diff |: {:.3f}'.format(name, val, grad, approx,
+                                    np.abs(grad - approx)))
