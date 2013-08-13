6c55c89e79ca7bd2cb919b6b5f1ab68f140bf82b
Dawen Liang <dl2771@columbia.edu>
2013-08-13 00:32:00 -0700

    complete the sanity check

--- code/exp_band_sanity.py
+++ code/exp_band_sanity.py
@@ -3,15 +3,17 @@
 
 # <codecell>
 
-import functools, glob, pickle
+import functools, glob, pickle, time
 
 import numpy as np
 import scipy.io as sio
+import scipy.stats as stats
 from scikits.audiolab import Sndfile, Format
 from matplotlib.pyplot import *
 
 import librosa
 import gamma_gvpl as vpl
+import sf_gap_nmf as sf_nmf
 
 # <codecell>
 
@@ -76,14 +78,23 @@ for file_dir in files[:N_train]:
             X_complex_train = librosa.stft(wav, n_fft=n_fft, hop_length=hop_length)
         else:
             X_complex_train = np.hstack((X_complex_train, librosa.stft(wav, n_fft=n_fft, hop_length=hop_length))) 
-#X_complex_test = None
-#for file_dir in files[N_train:]:
-#    for wav_dir in file_dir:
-#        wav, sr = load_timit(wav_dir)
-#        if X_complex_test is None:
-#            X_complex_test = librosa.stft(wav, n_fft=n_fft, hop_length=hop_length)
-#        else:
-#            X_complex_test = np.hstack((X_complex_test, librosa.stft(wav, n_fft=n_fft, hop_length=hop_length)))
+X_complex_test = None
+for file_dir in files[N_train:]:
+    for wav_dir in file_dir:
+        wav, sr = load_timit(wav_dir)
+        if X_complex_test is None:
+            X_complex_test = librosa.stft(wav, n_fft=n_fft, hop_length=hop_length)
+        else:
+            X_complex_test = np.hstack((X_complex_test, librosa.stft(wav, n_fft=n_fft, hop_length=hop_length)))
+
+# <codecell>
+
+# load the prior learned from training data
+d = sio.loadmat('priors/gamma_gender_batch.mat')
+U = d['U']
+gamma = d['gamma'].ravel()
+alpha = d['alpha'].ravel()
+L = alpha.size
 
 # <headingcell level=1>
 
@@ -94,31 +105,162 @@ for file_dir in files[:N_train]:
 # cut-off above 3000Hz
 freq_threshold = 3000.
 bin_cutoff = n_fft * freq_threshold / sr
-X_cutoff = X_complex_train[:(bin_cutoff+1)]
+X_cutoff_train = X_complex_train[:(bin_cutoff+1)]
 
-x_cutoff = librosa.istft(X_cutoff, n_fft=2*bin_cutoff, hop_length=bin_cutoff, hann_w=0)
-write_wav(x_cutoff, 'prior_be_cutoff.wav', samplerate=2 * freq_threshold)
-
-# <codecell>
-
-d = sio.loadmat('priors/gamma_gender_batch.mat')
-U = d['U']
-gamma = d['gamma'].ravel()
-alpha = d['alpha'].ravel()
-L = alpha.size
+x_cutoff_train = librosa.istft(X_cutoff_train, n_fft=2*bin_cutoff, hop_length=bin_cutoff, hann_w=0)
+write_wav(x_cutoff_train, 'prior_be_cutoff.wav', samplerate=2 * freq_threshold)
 
 # <codecell>
 
 reload(vpl)
-sf_encoder = vpl.SF_Dict(np.abs(X_cutoff.T), L=L, seed=98765)
-sf_encoder.U, sf_encoder.gamma, sf_encoder.alpha = U[:, :(bin_cutoff+1)], gamma[:(bin_cutoff+1)], alpha
+encoder_train = vpl.SF_Dict(np.abs(X_cutoff_train.T), L=L, seed=98765)
+encoder_train.U, encoder_train.gamma, encoder_train.alpha = U[:, :(bin_cutoff+1)], gamma[:(bin_cutoff+1)], alpha
 
-sf_encoder.vb_e(cold_start = False)
-A = sf_encoder.EA
+encoder_train.vb_e(cold_start = False)
+A = encoder_train.EA
 
 # <codecell>
 
-EX = np.zeros_like(np.abs(X_complex_train))
-for t in xrange(EX.shape[0]):
-    EX[t] = np.exp(np.sum(vpl.comp_log_exp(sf_encoder.a[t], sf_encoder.b[t], U), axis=0))
+EX_train = np.zeros_like(np.abs(X_complex_train.T))
+for t in xrange(EX_train.shape[0]):
+    EX_train[t] = np.exp(np.sum(vpl.comp_log_exp(encoder_train.a[t, :, np.newaxis], encoder_train.b[t, :, np.newaxis], U), axis=0))
+
+# <codecell>
+
+fig()
+subplot(121)
+specshow(logspec(EX_train.T))
+axhline(y=(bin_cutoff+1), color='black')
+colorbar()
+subplot(122)
+specshow(logspec(np.abs(X_complex_train)))
+axhline(y=(bin_cutoff+1), color='black')
+colorbar()
+pass
+
+# <codecell>
+
+## mean of predictive log-likelihood
+pred_likeli = np.mean(stats.expon.logpdf(np.abs(X_complex_train[(bin_cutoff+1):]), scale=EX_train[(bin_cutoff+1):]))
+print pred_likeli
+
+# <codecell>
+
+write_wav(librosa.istft(EX_train.T * (X_complex_train / np.abs(X_complex_train)), n_fft=n_fft, hann_w=0, hop_length=hop_length), 'be_sanity_check1.wav')
+
+# <headingcell level=1>
+
+# Sanity check 2: use the prior to fit the band-limited held-out data
+
+# <codecell>
+
+X_cutoff_test = X_complex_test[:(bin_cutoff+1)]
+
+x_cutoff_test = librosa.istft(X_test, n_fft=2*bin_cutoff, hop_length=bin_cutoff, hann_w=0)
+write_wav(x_cutoff_test, 'prior_be_cutoff.wav', samplerate=2 * freq_threshold)
+
+# <codecell>
+
+reload(vpl)
+encoder_test = vpl.SF_Dict(np.abs(X_cutoff_test.T), L=L, seed=98765)
+encoder_test.U, encoder_test.gamma, encoder_test.alpha = U[:, :(bin_cutoff+1)], gamma[:(bin_cutoff+1)], alpha
+
+encoder_test.vb_e(cold_start = False)
+A = encoder_test.EA
+
+# <codecell>
+
+EX_test = np.zeros_like(np.abs(X_complex_test))
+for t in xrange(EX_test.shape[0]):
+    EX_test[t] = np.exp(np.sum(vpl.comp_log_exp(encoder_test.a[t, :, np.newaxis], encoder_test.b[t, :, np.newaxis], U), axis=0))
+
+# <codecell>
+
+fig()
+subplot(121)
+specshow(logspec(EX_test.T))
+axhline(y=(bin_cutoff+1), color='black')
+colorbar()
+subplot(122)
+specshow(logspec(np.abs(X_complex_test)))
+axhline(y=(bin_cutoff+1), color='black')
+colorbar()
+pass
+
+# <codecell>
+
+## mean of predictive log-likelihood
+pred_likeli = np.mean(stats.expon.logpdf(np.abs(X_complex_test[(bin_cutoff+1):]), scale=EX_test[(bin_cutoff+1):]))
+print pred_likeli
+
+# <codecell>
+
+write_wav(librosa.istft(EX_test.T * (X_complex_test / np.abs(X_complex_test)), n_fft=n_fft, hann_w=0, hop_length=hop_length), 'be_sanity_check2.wav')
+
+# <headingcell level=1>
+
+# Sanity check 3: use the prior to fit the original band-limied training data under the full NMF setting
+
+# <codecell>
+
+reload(sf_nmf)
+U = d['U'].T
+sfnmf = sf_nmf.SF_GaP_NMF(np.abs(X_cutoff_train), U[:(bin_cutoff+1)], gamma[:(bin_cutoff+1)], alpha, K=100, seed=98765)
+
+# <codecell>
+
+score = sfnmf.bound()
+criterion = 0.0005
+objs = []
+for i in xrange(1000):
+    start_t = time.time()
+    sfnmf.update(disp=1)
+    t = time.time() - start_t
+    
+    lastscore = score
+    score = sfnmf.bound()
+    objs.append(score)
+    improvement = (score - lastscore) / abs(lastscore)
+    print ('iteration {}: bound = {:.2f} ({:.5f} improvement) time = {:.2f}'.format(i, score, improvement, t))
+    if improvement < criterion:
+        break
+
+# <codecell>
+
+plot(objs)
+pass
+
+# <codecell>
+
+## Infer the high-frequency contents
+goodk = sfnmf.goodk()
+Ew = np.zeros((U.shape[0], goodk.size))
+for (i, k) in enumerate(goodk):
+    Ew[:, i] = np.exp(np.sum(_gap.comp_log_exp(sfnmf.nua[:, k], sfnmf.rhoa[:, k], -U), axis=1))
+
+# <codecell>
+
+c = np.mean(sfnmf.X / sfnmf._xtwid(goodk))
+X_bar = c * np.dot(Ew * sfnmf.Et[goodk], sfnmf.Eh[goodk])
+
+fig()
+subplot(121)
+specshow(logspec(X_bar))
+axhline(y=(bin_cutoff+1), color='black')
+colorbar()
+subplot(122)
+specshow(logspec(np.abs(X_complex_train)))
+axhline(y=(bin_cutoff+1), color='black')
+colorbar()
+pass
+
+# <codecell>
+
+## mean of predictive log-likelihood
+pred_likeli = np.mean(stats.expon.logpdf(np.abs(X_complex_test[(bin_cutoff+1):]), scale=EX_test[(bin_cutoff+1):]))
+print pred_likeli
+
+# <codecell>
+
+write_wav(librosa.istft(X_bar * (X_complex_train / np.abs(X_complex_train)), n_fft=n_fft, hann_w=0, hop_length=hop_length), 'be_sanity_check3.wav')
 
