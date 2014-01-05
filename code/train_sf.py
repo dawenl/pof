7bb4937a7870086cac425c383e53ba5ded65429b
Dawen Liang <dl2771@columbia.edu>
2014-01-05 08:59:40 -0500

    merge

--- code/train_sf.py
+++ code/train_sf.py
@@ -11,23 +11,21 @@ def train_sf(matfile, L, threshold=0.0005, maxiter=200):
     W = d['W']
 
     old_obj = -np.inf
-    cold_start = False
-    batch_m = True
 
     sfd = vpl.SF_Dict(W.T, L=L, seed=98765)
     for i in xrange(maxiter):
-        sfd.vb_e(cold_start=cold_start, disp=1)
-        sfd.vb_m(batch=batch_m, disp=1)
+        sfd.vb_e(disp=1)
+        sfd.vb_m(disp=1)
         score = sfd.bound()
         improvement = (score - old_obj) / abs(old_obj)
         print('After ITERATION: {}\tObjective: {:.2f}\t''Old objective: '
               '{:.2f}\tImprovement: {:.4f}'.format(i, score, old_obj,
                                                    improvement))
+	sio.savemat('sf_L{}_{}.iter{}'.format(L, matfile, i),
+	            {'U': sfd.U, 'alpha': sfd.alpha, 'gamma': sfd.gamma})
         if improvement < threshold:
             break
         old_obj = score
-    sio.savemat('sf_L{}_{}'.format(L, matfile),
-                {'U': sfd.U, 'alpha': sfd.alpha, 'gamma': sfd.gamma})
     pass
 
 if __name__ == '__main__':
